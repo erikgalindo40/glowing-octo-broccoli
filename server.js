4919bc36564bb814bd955186874ccd6fc28df258
erikgalindo40 <erikgalindo40@yahoo.com>
2022-11-21 10:51:08 -0800

    add CRUD routes to server

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-app.use('/',(req,res)=>{
-    console.log(res)
-    res.send('server connected again!')
+app.get('/',(req,res)=>{
+    res.json({text: 'data retrieved'})
+})
+app.post('/', (req,res)=>{
+    res.json({text: 'data created'})
+})
+app.put('/:id', (req,res)=>{
+    // console.log(req.params);
+    res.json({text: `data ${req.params.id} updated`})
+})
+app.delete('/:id', (req,res)=>{
+    res.json({text: `data ${req.params.id} deleted`})
 })
 
 app.listen(PORT)
